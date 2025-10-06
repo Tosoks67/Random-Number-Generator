b6b977f6bca6224e91bcdee4ef3231b059d3ced7
Tosoks <126388095+Tosoks67@users.noreply.github.com>
2025-10-06 23:24:03 +0200

    Add files via upload

---
+++ random_import.js
@@ -0,0 +1,8 @@
+export function random(num1, num2) {
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    if (isNaN(num1) || isNaN(num2)) return 0;
+    if (num1 > num2) return 0;
+    if (num1 === num2) return num1;
+    return Math.floor(Math.random() * (num2 - num1 + 1)) + num1;
+}
