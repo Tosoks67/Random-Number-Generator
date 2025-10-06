e8a464c43d7eeb455e8ec411e34da41b69f4a65f
Tosoks <126388095+Tosoks67@users.noreply.github.com>
2025-10-06 10:18:03 +0200

    Update random.js

--- random.js
+++ random.js
@@ -5,6 +5,6 @@ function random(num1, num2) {
     if (num1 > num2) return 0;
     if (num1 === num2) return num1;
     return Math.floor(Math.random() * (num2 - num1 + 1)) + num1;
-
 }
 
+window.random = random;
